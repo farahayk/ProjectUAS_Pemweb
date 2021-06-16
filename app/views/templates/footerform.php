e6c15093da3a6baa9e939c9ad33a2ce9cf9124cb
evinafiahh <72215977+evinafiahh@users.noreply.github.com>
2021-06-16 15:48:34 +0700

    bismillah sampe assets
    bismillah sampe assets

---
+++ app/views/templates/footerform.php
@@ -0,0 +1,5 @@
+    <!-- JS -->
+    <script src="http://localhost/ProjectUAS_Pemweb/public/form/vendor/jquery/jquery.min.js"></script>
+    <script src="http://localhost/ProjectUAS_Pemweb/public/form/js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
---
+++ app/views/templates/headerform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $data['judul']; ?></title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="http://localhost/ProjectUAS_Pemweb/public/form/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="http://localhost/ProjectUAS_Pemweb/public/form/css/style.css">
+</head>
+<body>
